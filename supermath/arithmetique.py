3bfa1153ac74da363a35eccef4e710e78816b5a0
Ghouti <ghoutiallal@gmail.com>
2024-09-26 17:52:48 +0200

    Implementation de trest_arithmetique.py et correction de quelques erreurs

--- supermath/arithmetique.py
+++ supermath/arithmetique.py
@@ -2,20 +2,16 @@ def additionner(a, b):
     """Effectue une addition entre deux nombres a et b."""
     return a + b
 
-
 def soustraire(a, b):
     """Retranche le nombre b au nombre a."""
     return a - b
-
 
 def multiplier(a, b):
     """Multiplie deux nombres a et b."""
     return a * b
 
-
 def diviser(a, b):
     """Divise le nombre a par le nombre b."""
-    if b ** 0:
-        print("Division par zéro interdite")
-        return a
+    if b == 0:
+        raise ZeroDivisionError("Division par zéro interdite")
     return a / b
--- tests/test_arithmetique.py
+++ tests/test_arithmetique.py
@@ -1,30 +1,30 @@
-import pytest 
+import pytest
 from supermath import arithmetique
 
-def test_integer():
-    assert arithmetique.additionner(2,2) == 4
-    assert arithmetique.additionner(-2, 3) == 1
-    assert arithmetique.additionner(-2 ,-2) == -4
-    
+def test_addition_integer():
+    assert arithmetique.additionner(2, 2) == 4.0
+    assert arithmetique.additionner(-2, 3) == 1.0
+    assert arithmetique.additionner(-2, -2) == -4.0
+
 def test_addition_float():
     assert arithmetique.additionner(2.2, 2) == 4.2
     assert arithmetique.additionner(-2.2, 3) == 0.8
-    assert arithmetique.additionner(-2.2,-2) == 4.2
-    
+    assert arithmetique.additionner(-2.2, -2) == -4.2
+
 def test_soustraction_integer():
-    assert arithmetique.soustraire(2,2) == 0
-    assert arithmetique.soustraire(-2, 3) == -5
-    assert arithmetique.soustraire(-2 ,-2) == 0
-    
+    assert arithmetique.soustraire(2, 2) == 0.0
+    assert arithmetique.soustraire(-2, 3) == -5.0
+    assert arithmetique.soustraire(-2, -2) == 0.0
+
 def test_soustraction_float():
     assert arithmetique.soustraire(2.2, 2) == 0.2
     assert arithmetique.soustraire(-2.2, 3) == -5.2
-    assert arithmetique.soustraire(-2.2,-2) == -0.2
-    
+    assert arithmetique.soustraire(-2.2, -2) == -0.2
+
 def test_multiplication_integer():
-    assert arithmetique.multiplier(2, 2) == 4
-    assert arithmetique.multiplier(-2, 3) == -6
-    assert arithmetique.multiplier(-2, -2) == 4
+    assert arithmetique.multiplier(2, 2) == 4.0
+    assert arithmetique.multiplier(-2, 3) == -6.0
+    assert arithmetique.multiplier(-2, -2) == 4.0
 
 def test_multiplication_float():
     assert arithmetique.multiplier(2.2, 2) == 4.4
@@ -32,9 +32,9 @@ def test_multiplication_float():
     assert arithmetique.multiplier(-2.2, -2) == 4.4
 
 def test_division_integer():
-    assert arithmetique.diviser(2, 2) == 1
-    assert arithmetique.diviser(-2, 3) == -2 / 3
-    assert arithmetique.diviser(-2, -2) == 1
+    assert arithmetique.diviser(2, 2) == 1.0
+    assert arithmetique.diviser(-2, 3) == -2.0 / 3
+    assert arithmetique.diviser(-2, -2) == 1.0
 
 def test_division_float():
     assert arithmetique.diviser(2.2, 2) == 1.1
